b12be0eb2f365cae6ef1d09f7eef0bccef420ffc
Brandon Wright <55092542+Barnold8@users.noreply.github.com>
2021-09-02 17:47:33 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,23 @@
+/* This C program is just a simple calculator */
+
+
+#include "Calc.h"
+
+int main(void){
+
+	struct calculator f = {0,0};
+
+	mainLoop(&f.num1,&f.num2);
+
+	return 0;
+} 
+
+
+/* Comment section
+
+	
+
+	printf("%f",f.num1);
+
+
+*/
